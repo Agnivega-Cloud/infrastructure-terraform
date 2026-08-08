7470f839593e530c0c8503e2ce2775c74cb32c4b
Vibhakar <vibhakar246@gmail.com>
2026-08-08 09:02:08 +0000

    Create public and private subnets

--- main.tf
+++ main.tf
@@ -9,3 +9,24 @@ resource "aws_vpc" "main" {
     Project     = "Sprint-1"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "agnivega-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "agnivega-private-subnet"
+  }
+}
