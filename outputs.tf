50dabccd0f3026d8b12fe9fe7ff307c2f6ae71bc
Vibhakar <vibhakar246@gmail.com>
2026-08-09 18:40:54 +0000

    Create public route table and association

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "public_subnet_id" {
 output "private_subnet_id" {
   value = aws_subnet.private.id
 }
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
