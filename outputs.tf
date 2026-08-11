076a5e9470a73ff5de14c14f71b8f577f5279497
Vibhakar <vibhakar246@gmail.com>
2026-08-11 15:40:43 +0000

    feat: provision bastion host

--- outputs.tf
+++ outputs.tf
@@ -10,7 +10,17 @@ output "private_subnet_id" {
   value = aws_subnet.private.id
 }
 
+output "bastion_id" {
+  value = aws_instance.bastion.id
+}
 
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "bastion_security_group_id" {
+  value = aws_security_group.bastion.id
+}
 
 output "web_security_group_id" {
   value = aws_security_group.web.id
